cd529892a2dbec94baa884a16b365f132d7b9fb5
Rachel Kim <hyunjinney@naver.com>
2022-07-25 00:35:58 +0900

    1st_test commit_
    database 추가
    CREATE TABLE scheTBL (dateName STRING PRIMARY KEY, timeData STRING, myplans CHAR(30)

--- app/src/main/java/org/techtown/scheduling/MainActivity.kt
+++ app/src/main/java/org/techtown/scheduling/MainActivity.kt
@@ -8,15 +8,24 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var myHelper : myDBHelper
+    lateinit var sqlDB:SQLiteDatabase
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_sche_day)
+
+        myHelper = myDBHelper(this)
+        //db생성해서 초기화시키는 코드
+        sqlDB = myHelper.writableDatabase
+        myHelper.onUpgrade(sqlDB,1,2)
+        sqlDB.close()
     }
 
     inner class myDBHelper(context: Context):SQLiteOpenHelper(context,"scheDB",null,1){
         override fun onCreate(db: SQLiteDatabase?) {
-            db!!.execSQL("CREATE TABLE scheTBL (weekName STRING PRIMARY KEY, gNumber INTEGER);")
+            db!!.execSQL("CREATE TABLE scheTBL (dateName STRING PRIMARY KEY, timeData STRING, myplans CHAR(30));")
         }
 
         override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
